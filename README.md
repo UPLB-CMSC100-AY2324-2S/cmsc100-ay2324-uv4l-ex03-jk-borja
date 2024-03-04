10ec59cc60c9fbd67754ddca9f201fca77957de4
jk-borja <jbborja@up.edu.ph>
2024-03-04 13:49:50 +0800

    chore: edited README file and added initial exer03_borja.js file

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Title: Simulate a password validation program in JavaScript
 Type: Individual Assessment
 Score: (15 points)
 
+**Name:** Borja, Julia Katharine B. <br/>
+**Student number:** 2021-00419 <br/>
+**Section:** UV4L <br/>
+**Degree Program:** BS Computer Science
 
---
+++ exer03_borja.js
@@ -0,0 +1,17 @@
+function validatePassword(pass1, pass2){
+
+}
+
+function reversePassword(password){
+
+}
+
+function storePassword(username, pass1, pass2){
+   
+}
+
+validatePassword("helloworld", "hello") // returns false
+validatePassword("hello", "hello") // returns false
+validatePassword("hello1234", "hello1234") // returns false
+validatePassword("Hello1234", "Hello1234") // returns true
+validatePassword("HELLO1234", "HELLO1234") // returns false
